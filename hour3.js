b01a021aa4e82f2d5f7dab51acb8c2258f0027ec
Marigowda, Santosh <santosharakere@gmail.com>
2016-09-04 10:37:30 +0100

    default parameter

---
+++ hour3.js
@@ -0,0 +1,10 @@
+// default parameters
+function foo(x) {
+  x = x || 42  // assiging default value to a parameter
+  return x
+}
+
+
+// console.log(foo())
+console.log(foo(0))
+// console.log(foo(30))
